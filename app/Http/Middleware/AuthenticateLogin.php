136c9e515b92cdb8c94436236f23303f4d8adf9b
MahmutSamii <samimahmut34@gmail.com>
2022-11-02 11:58:43 +0300

    init laravel.

---
+++ app/Http/Middleware/AuthenticateLogin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+
+class AuthenticateLogin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return string
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (Auth::user()){
+            return route('admin');
+        }
+    }
+}
